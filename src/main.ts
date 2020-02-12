b926339a80228358923f91cbbbb549ef5d99220b
Tim Rybicki <t.rybicki@gmail.com>
2020-02-11 20:25:12 -0500

    Add icon

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,46 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, Menu, Tray } from 'electron';
 import * as path from 'path';
 
-let mainWindow: Electron.BrowserWindow;
-
-function createWindow() {
-  mainWindow = new BrowserWindow({
-    height: 600,
-    width: 800,
-  });
-
-  mainWindow.loadFile(path.join(__dirname, '../index.html'));
-  mainWindow.webContents.openDevTools();
-
-  // Dereference the window object
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
-}
-
-app.on('ready', createWindow);
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+let tray = null;
+app.on('ready', () => {
+  tray = new Tray(path.join(__dirname, '../icon.png'));
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Item1', type: 'radio' },
+    { label: 'Item2', type: 'radio' },
+    { label: 'Item3', type: 'radio', checked: true },
+    { label: 'Item4', type: 'radio' },
+  ]);
+  tray.setToolTip('This is my application.');
+  tray.setContextMenu(contextMenu);
 });
 
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
+// let mainWindow: Electron.BrowserWindow;
+
+// function createWindow() {
+//   mainWindow = new BrowserWindow({
+//     height: 600,
+//     width: 800,
+//   });
+
+//   mainWindow.loadFile(path.join(__dirname, '../index.html'));
+//   mainWindow.webContents.openDevTools();
+
+//   // Dereference the window object
+//   mainWindow.on('closed', () => {
+//     mainWindow = null;
+//   });
+// }
+
+// app.on('ready', createWindow);
+
+// app.on('window-all-closed', () => {
+//   if (process.platform !== 'darwin') {
+//     app.quit();
+//   }
+// });
+
+// app.on('activate', () => {
+//   if (mainWindow === null) {
+//     createWindow();
+//   }
+// });
